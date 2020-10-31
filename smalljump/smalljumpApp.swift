788e0023fe83cfa1ce9f504e6edd59a12d237180
pianostringquartet <cjc500@nyu.edu>
2020-10-31 09:45:29 -0600

    Initial Commit

---
+++ smalljump/smalljumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  smalljumpApp.swift
+//  smalljump
+//
+//  Created by cjc on 10/31/20.
+//
+
+import SwiftUI
+
+@main
+struct smalljumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
