94cd87849566d09c11559b681c22a0aeadb4541d
pianostringquartet <cjc500@nyu.edu>
2020-11-01 14:55:59 -0700

    Bouncy balls

--- smalljump/ContentView.swift
+++ smalljump/ContentView.swift
@@ -7,114 +7,78 @@
 
 import SwiftUI
 
-func ball(top: Color, bottom: Color, width: CGFloat, height: CGFloat) -> some View {
-    LinearGradient(gradient: Gradient(colors: [top, bottom]), startPoint: .topLeading, endPoint: .bottomTrailing)
-        // width and height are SAME for circle...
-        .frame(width: width, height: height)
+func coloredCircle(color: Color, radius: CGFloat) -> some View {
+    LinearGradient(gradient: Gradient(colors: [.white, color]),
+                   startPoint: .topLeading,
+                   endPoint: .bottomTrailing)
+        .frame(width: radius, height: radius)
         .clipShape(Circle())
 }
 
-struct ColoredCircle: View {
-
-    @State private var dragged = CGSize.zero
-    @State private var accumulated = CGSize.zero
-
-    let topColor, bottomColor: Color
+// ball's new position = old position + displacement from current drag gesture
+func updatePosition(value: DragGesture.Value, position: CGSize) -> CGSize {
+    CGSize(width: value.translation.width + position.width,
+           height: value.translation.height + position.height)
+}
+    
+// A ball that stays where you drag it
+struct DraggableBall: View {
+    
+    // how far displaced in given gesture
+    @State private var position = CGSize.zero
+    
+    @State private var previousPosition = CGSize.zero
+        
+    let color: Color
     let radius: CGFloat
     
-    init(topColor: Color, bottomColor: Color, radius: CGFloat) {
-        self.topColor = topColor
-        self.bottomColor = bottomColor
-        self.radius = radius
-    }
-    
     var body: some View {
-    
-        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            .frame(width: radius, height: radius, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)//.frame(width: 60, height: 60) //.border(Color.red)
-            .offset(x: self.dragged.width, y: self.dragged.height)
-            .animation(.default)
+        coloredCircle(color: color, radius: radius)
+            
+            // move ball as we drag
+            .offset(x: self.position.width, y: self.position.height)
+            
+            // alternatively: move ball only after we let go
+//            .offset(x: self.previousPosition.width, y: self.previousPosition.height)
+            
             .gesture(DragGesture()
-                .onChanged{ value in
-                    self.dragged = CGSize(width: value.translation.width + self.accumulated.width, height: value.translation.height + self.accumulated.height)
-                    
-                }
-                .onEnded{ value in
-                    self.dragged = CGSize(width: value.translation.width + self.accumulated.width, height: value.translation.height + self.accumulated.height)
-                    self.accumulated = self.dragged
-                }
-            )
+                        .onChanged { self.position = updatePosition(value: $0, position: self.previousPosition) }
+                        .onEnded { _ in self.previousPosition = self.position })
+            
+            // give ball some bounce
+            .animation(.spring(response: 0.3, dampingFraction: 0.65, blendDuration: 4))
     }
 }
 
-struct MovingCircle: View {
 
-    @State private var dragged = CGSize.zero
-    @State private var accumulated = CGSize.zero
-
-    var body: some View {
-        
-        // solve the 'reset problem' by:
-        // keeping track of how much we've moved
-        
-        
-        // note: CGSize has two dimensions: width, height
-        // onChange and onEnded callbacks receive a value
-        
+// A ball that bounces back to its original position
+struct BoomerangBall: View {
     
-        LinearGradient(gradient: Gradient(colors: [Color.yellow, Color.green]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            .frame(width: 60, height: 60) //.border(Color.red)
-            
-            // what is the offset, such that it would be the `dragged`, rather than the `accumulated`?
-            .offset(x: self.dragged.width, y: self.dragged.height)
-        
-            // animation here vs after .gesture?
-            .animation(.default)
-            
-            .gesture(DragGesture()
-                .onChanged{ value in
-                    // while we're dragging, we only modify `dragged`
-                    self.dragged = CGSize(width: value.translation.width + self.accumulated.width, height: value.translation.height + self.accumulated.height)
-            }
-            .onEnded{ value in
-//                    once we've finished dragging, we update the
-                self.dragged = CGSize(width: value.translation.width + self.accumulated.width, height: value.translation.height + self.accumulated.height)
-                self.accumulated = self.dragged
-                }
-            )
+    @State private var position = CGSize.zero
+    
+    let color: Color
+    let radius: CGFloat
+    
+    var body: some View {
+        coloredCircle(color: .black, radius: 125)
+        .offset(position)
+        .gesture(DragGesture()
+                    .onChanged { self.position = $0.translation }
+                    .onEnded { _ in
+                        withAnimation(.spring()) { self.position = .zero }
+                    })
     }
 }
 
 
 struct ContentView: View {
-    @State private var scale: CGFloat = 1.0
-
-//    local state to store amount of drag
-    // added CGSize
-    @State private var dragAmount: CGSize = CGSize.zero
-    @State private var dragAmount2: CGSize = CGSize.zero
-    @State private var dragAmount3: CGSize = CGSize.zero
-
-    @State private var goalPipeline: String = "goal: f g h"
-    @State private var currentPipeline: String = "current: "
-
+    
     var body: some View {
         VStack {
-            ball(top: Color.white, bottom: Color.black, width: 75, height: 125)
-                .offset(dragAmount3)
-                .gesture(DragGesture()
-                            .onChanged { self.dragAmount3 = $0.translation }
-                            .onEnded { _ in
-                                withAnimation(.spring()) { self.dragAmount3 = .zero }
-                            })
-            ColoredCircle(topColor: .yellow, bottomColor: .green, radius: 75)
-            ColoredCircle(topColor: .white, bottomColor: .pink, radius: 75)
-
-            // TODO: needs to be placed at very bottom of screen,
-            Text(goalPipeline).bold().font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            Text(currentPipeline).bold().font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            BoomerangBall(color: .black, radius: 125)
+            DraggableBall(color: .red, radius: 100)
+            DraggableBall(color: .blue, radius: 75)
+            DraggableBall(color: .green, radius: 50)
         }
     }
 }
