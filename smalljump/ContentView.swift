eda8fee1f81091717de9eec590a611a43666799f
pianostringquartet <cjc500@nyu.edu>
2020-11-01 19:27:20 -0700

    Edge updates as balls move

--- smalljump/ContentView.swift
+++ smalljump/ContentView.swift
@@ -82,17 +82,44 @@ struct StrangeShape: View {
     }
 }
 
+// Extending CGPoint to work with VectorArithmetic, needded
+extension CGPoint: VectorArithmetic {
+    public static func -= (lhs: inout CGPoint, rhs: CGPoint) {
+        lhs = lhs - rhs
+    }
+    
+    public static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+    
+    public static func += (lhs: inout CGPoint, rhs: CGPoint) {
+        lhs = lhs + rhs
+    }
+    
+    public mutating func scale(by rhs: Double) {
+        x *= CGFloat(rhs)
+        y *= CGFloat(rhs)
+    }
+    
+    public static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+    
+    public var magnitudeSquared: Double { return Double(x*x + y*y) }
+}
+
+// same as EdgeShape used here: https://gist.github.com/chriseidhof/d23f82f8a9e85e75bc02be220326199a
 struct Line: Shape {
     var from: CGPoint
     var to: CGPoint
     
-//    var animatableData: AnimatablePair<CGPoint, CGPoint> {
-//        get { AnimatablePair(from, to) }
-//        set {
-//            from = newValue.first
-//            to = newValue.second
-//        }
-//    }
+    var animatableData: AnimatablePair<CGPoint, CGPoint> {
+        get { AnimatablePair(from, to) }
+        set {
+            from = newValue.first
+            to = newValue.second
+        }
+    }
 
     func path(in rect: CGRect) -> Path {
         Path { p in
@@ -101,6 +128,24 @@ struct Line: Shape {
         }
     }
 }
+
+class Unique<A>: Identifiable {
+    let value: A
+    init(_ value: A) { self.value = value }
+}
+
+extension Unique: Equatable where A: Equatable {
+    static func == (lhs: Unique<A>, rhs: Unique<A>) -> Bool {
+        return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+}
+extension Unique: Hashable where A: Hashable {
+    func hash(into hasher: inout Hasher) {
+        value.hash(into: &hasher)
+    }
+}
+
+
 
 
 // defining a data type for a preference data
@@ -143,12 +188,15 @@ struct EdgeBall: View {
             .animation(.spring(response: 0.3, dampingFraction: 0.65, blendDuration: 4))
             
 //            modeled on: https://swiftui-lab.com/communicating-with-the-view-tree-part-2/
+//            .anchorPreference(key: MyAnchorPreferenceKey.self,
+//                              value: .center, // center for CGPoint
+//                              transform: { [MyAnchorPreferenceData(viewIdx: self.idx, center: $0)] })
             
             // the child will set its anchor preference data,
 //          i.e. it will give back its own .center point
-            .anchorPreference(key: MyAnchorPreferenceKey.self,
-                              value: .center, // center for CGPoint
-                              transform: { [MyAnchorPreferenceData(viewIdx: self.idx, center: $0)] })
+//            .anchorPreference(key: MyAnchorPreferenceKey.self,
+//                              value: .center, // center for CGPoint
+//                              transform: { [MyAnchorPreferenceData(viewIdx: self.idx, center: $0)] })
     }
 }
 
@@ -156,6 +204,22 @@ struct EdgeBall: View {
 struct ContentView: View {
     
     @State private var activeIdx: Int = 0
+    
+    @State private var cgp0: CGPoint = CGPoint(x: 100, y: 200)
+    @State private var cgp1: CGPoint = CGPoint(x: 200, y: 100)
+    
+    
+    @State private var position: CGSize = CGSize.zero
+    @State private var previousPosition: CGSize = CGSize.zero
+    let color: Color = Color.green
+    let radius: CGFloat = 50
+    
+    @State private var position1 = CGSize.zero
+    @State private var previousPosition1 = CGSize.zero
+    let color1 = Color.blue
+    let radius1: CGFloat = 75
+    
+    
     
     // for easier visibility of edges
     private var spacing: CGFloat = 50
@@ -166,35 +230,91 @@ struct ContentView: View {
 //            DraggableBall(color: .blue, radius: 75)
 //            DraggableBall(color: .green, radius: 50)
             
-            EdgeBall(idx: 0, color: .purple, radius: 75)
-            EdgeBall(idx: 1, color: .pink, radius: 50)
+            // .map(Unique.init)
+            EdgeBall(idx: 0, color: .purple, radius: 75).onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+                self.cgp1 = CGPoint(x: 50, y: 75)
+            })
+            EdgeBall(idx: 1, color: .pink, radius: 50).onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+                self.cgp0 = CGPoint(x: 150, y: 170)
+            })
+            
+//            Line(from: cgp0, to: cgp1).stroke()
             
             // can create something better...
 //            Line(from: CGPoint(x: 100, y: 200), to: CGPoint(x: 200, y: 100)).stroke()
-//            StrangeShape()
-        }.backgroundPreferenceValue(MyAnchorPreferenceKey.self) { preferences in
-            return GeometryReader { geometry in
-//                ForEach(preferences )
+            
+            
+        
+            coloredCircle(color: color, radius: radius)
+                    .anchorPreference(key: MyAnchorPreferenceKey.self,
+                              value: .center, // center for CGPoint
+                              transform: { [MyAnchorPreferenceData(viewIdx: 0, center: $0)] })
+                    .offset(x: self.position.width, y: self.position.height)
+                    .gesture(DragGesture()
+                                .onChanged { self.position = updatePosition(value: $0, position: self.previousPosition) }
+                                .onEnded { _ in self.previousPosition = self.position })
+                    .animation(.spring(response: 0.3, dampingFraction: 0.65, blendDuration: 4))
+                    
                 
-                // preferences is the PreferencesKeyData, no?
-                
-//                Line
-                
-                let p0 = preferences.first(where: { $0.viewIdx == 0})
-                let p1 = preferences.first(where: { $0.viewIdx == 1})
+            
+            
+            coloredCircle(color: color1, radius: radius1)
+                        .anchorPreference(key: MyAnchorPreferenceKey.self,
+                          value: .center, // center for CGPoint
+                          transform: { [MyAnchorPreferenceData(viewIdx: 1, center: $0)] })
+                        .offset(x: self.position1.width, y: self.position1.height)
+                        .gesture(DragGesture()
+                                    .onChanged { self.position1 = updatePosition(value: $0, position: self.previousPosition1) }
+                                    .onEnded { _ in self.previousPosition1 = self.position1 })
+                        .animation(.spring(response: 0.3, dampingFraction: 0.65, blendDuration: 4))
+                        
+            
+//            position.
+            
+            
+            
+            // this line works, and changes as we drag the balls
+//            Line(from: CGPoint(x: position.width, y: position.height),
+//                 to: CGPoint(x: position1.width, y: position1.height))
+//                .stroke()
+            
+            // however, the alignment is bad
+            //
+         
+//            GeometryReader { geometry in
 //
+//                let globalMidX = geometry.frame(in: .global).midX
+//                let globalMidY = geometry.frame(in: .global).midY
+//
+//                let gp = CGPoint(x: globalMidX, y: geometry.frame(in: .global).midY)
+//                let gpTop = CGPoint(x: geometry.frame(in: .local).midX, y: geometry.frame(in: .local).midY)
+//
+//
+//                Line(from: gp, to: gpTop).stroke().animation(.default)
+//
+////                let point0 = geometry[position]
+////                let point1 = geometry[position1]
+////
+////                Line(from: point0, to: point1).stroke().animation(.default)
+//            }
+            
+        }
+        
+        // works BUT DOES NOT DETECT MOVEMENT OF BALLS
+        .backgroundPreferenceValue(MyAnchorPreferenceKey.self) { preferences in
+            return GeometryReader { geometry in
                 
-                let point0 = geometry[p0!.center]
-                let point1 = geometry[p1!.center]
+//                let p0 = preferences.first(where: { $0.viewIdx == 0})
+//                let p1 = preferences.first(where: { $0.viewIdx == 1})
+//                let point0 = geometry[p0!.center]
+//                let point1 = geometry[p1!.center]
                 
-                Line(from: point0, to: point1).stroke()
-                
-//                return Line(from: preferences[0], to: preferences[1]).stroke()
-                
-                
-//                self.createBorder(geometry, preferences)
-//                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                
+                let p0 = preferences[0]
+                let p1 = preferences[1]
+                // the ! is an unwrapped from an optional type?
+                let point0 = geometry[p0.center]
+                let point1 = geometry[p1.center]
+                Line(from: point0, to: point1).stroke().animation(.default)
             }
         }
     }
