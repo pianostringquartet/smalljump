44c0decdcdce22dea9e0e73c3d25ad741ad4bc12
pianostringquartet <cjc500@nyu.edu>
2020-11-02 22:41:04 -0700

    Fix bugs via clearer rules

--- smalljump/ContentView.swift
+++ smalljump/ContentView.swift
@@ -109,7 +109,7 @@ struct EdgeBall: View {
     }
      
     var body: some View {
-        let existsConnectingNode: Bool = self.connectingNode != nil
+        let isEditMode: Bool = self.connectingNode != nil
         let isConnectingNode: Bool = self.idx == self.connectingNode
         
         coloredCircle(color: isConnectingNode ? Color.green : (isEnabled ? Color.red : Color.gray),
@@ -125,50 +125,31 @@ struct EdgeBall: View {
                         .onChanged { self.position = updatePosition(value: $0, position: self.previousPosition) }
                         .onEnded { _ in self.previousPosition = self.position })
             .animation(.spring(response: 0.3, dampingFraction: 0.65, blendDuration: 4))
-             // Note: double-tap gesture must come before single-tap gesture
-            .onTapGesture(count: 2, perform: {
-                            // any time we double click the node, we put it
-                            
-                            // possible states:
-                            // edit mode: i'm cnode and i was enabled
-                            // edit mode: i'm cnode and i was disabled
-                            // edit mode: i'm not cnode and i was disabled
-                            
-                            // i.e. if there was no existing cnode, and I'm disabled, re
-                            if !existsConnectingNode && !isEnabled {
-                                self.connectingNode = self.idx
+             // Note: double-tap gesture modifier must come before single-tap gesture modifier
+            .onTapGesture(count: 2,
+                          perform: {
+                            // If I'm the connecting node, turn edit mode off and set me as 'enabled'
+                            if isConnectingNode {
+                                self.connectingNode = nil
                                 self.isEnabled = true
                             }
-                            
-                            if isConnectingNode && !isEnabled {
-                                // automatically enable nodes that become the connecting node,
-//                                self.isEnabled.toggle()
-                                self.isEnabled = true
-                            }
-                            
-                            if existsConnectingNode && isConnectingNode {
-                                log("edit mode: this node \(self.idx) is connecting node")
-                                self.connectingNode = nil // ie user has toggled connecting mode off
-                                // and re-enable the node:
-                                self.isEnabled = true
-                            }
+                            // If I'm not the connecting node, make me the connecting node and set me as 'enabled'
                             else {
                                 self.connectingNode = self.idx
+                                self.isEnabled = true
                             }})
-            
-            .onTapGesture(count: 1, perform: {
-                //
-                
-                if !existsConnectingNode { // if we're not in editing mode, enable/disable this node
-                    log("not in edit mode: this node \(self.idx) will be enabled/disabled")
-                    self.isEnabled.toggle()
-                }
-                // if we're in editing mode and this node is enabled:
-                else if existsConnectingNode && isEnabled {
-                    let newConnection: Connection = Connection(from: self.connectingNode!, to: self.idx)
-                    self.connections.append(newConnection)
-                }
-                // else: if we're in editing mode but this node is disabled, don't create a new connection
+            .onTapGesture(count: 1,
+                          perform: {
+                            // If we're not in edit mode, enable/disable this node
+                            if !isEditMode {
+                                self.isEnabled.toggle()
+                            }
+                            // If we're in editing mode and this node is enabled:
+                            else if isEditMode && isEnabled {
+                                let newConnection: Connection = Connection(from: self.connectingNode!, to: self.idx)
+                                self.connections.append(newConnection)
+                            }
+                            // else: if we're in editing mode but this node is disabled, don't create a new connection
             })
     }
 }
@@ -213,34 +194,10 @@ struct ContentView: View {
                     ForEach(connections, content: { (connection: Connection) in
                         let toPref: BallPreferenceData = preferences[connection.to]
                         let fromPref: BallPreferenceData = preferences[connection.from]
-                        
-                        // TODO: Simplify this logic
-//                            better?:
-                        log("connection.to: \(connection.to)")
-                        log("connection.from: \(connection.from)")
-                        log("toPref.isEnabled: \(toPref.isEnabled)")
-                        log("fromPref.isEnabled: \(fromPref.isEnabled)")
-                        
-                        let cannotDraw: Bool = !toPref.isEnabled || !fromPref.isEnabled
-                        log("cannotDraw: \(cannotDraw)")
-                        
-                        
-                        // if one of them is disabled
-                        
-                        
-                        if !cannotDraw {
+                        if toPref.isEnabled && fromPref.isEnabled {
                             line(from: geometry[toPref.center],
                                  to: geometry[fromPref.center])
                         }
-                        
-//                        let canDrawConnection: Bool = (toPref.isEnabled && fromPref.isEnabled)
-//                            || (connection.to == connectingNode && fromPref.isEnabled)
-//                            || (connection.from == connectingNode && toPref.isEnabled)
-//
-//                        if canDrawConnection {
-//                            line(from: geometry[toPref.center],
-//                                 to: geometry[fromPref.center])
-//                        }
                     })
                 }
             }
